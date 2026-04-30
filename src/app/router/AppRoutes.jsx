816c1da3bac28be64c353ec751810aa0959d6d68
Marioescobar64 <mescobar-2022471@kinal.edu.gt>
2026-04-30 16:42:24 -0600

    fix/ arreglos de clases inesesarias y arreglos de errores

--- src/app/router/AppRoutes.jsx
+++ src/app/router/AppRoutes.jsx
@@ -1,16 +1,35 @@
-import {Route, Routes} from "react-router-dom";
+import { Navigate, Routes, Route } from "react-router-dom";
 import { AuthPage } from "../../features/auth/pages/AuthPage";
 import { DashboardPage } from "../layaouts/DashboardPage";
+
+const RequireAdmin = ({ children }) => {
+  const role = localStorage.getItem("userRole") ?? "";
+  const isAdmin = role.toUpperCase().includes("ADMIN");
+
+  if (!isAdmin) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
 
 export const AppRoutes = () => {
     return(
         <Routes>
             {/* PUBLIC */}
             <Route path="/" element={<AuthPage />} />
+                <Route path="/login" element={<Navigate to="/" replace />} /> // aqui me da error
 
             {/* PROTEGIDO POR ROLE */}
-            <Route path="/dashboard/*" element={<DashboardPage />} />
+            <Route path="/dashboard/*" element={ <RequireAdmin>
+            <DashboardPage />
+          </RequireAdmin>} />
 
+                {/* Aqui van las rutas internas del dashboard */}
+
+
+
+         <Route path="*" element={<h1>Página no encontrada</h1>} />
         </Routes>
     );
 };
